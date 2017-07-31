d53b78ddb678d243997bbd7127cf05fa817fc6bf
isMyShell <1037822795@qq.com>
2017-07-31 14:24:30 +0800

    歌手数据获取

---
+++ vue-music/src/common/js/singer.js
@@ -0,0 +1,8 @@
+export default class Singer {
+	constructor({id, name , mid}) {
+		this.Fsinger_id = id
+		this.Fsinger_name = name
+		this.Fsinger_mid = mid
+		this.img = `https://y.gtimg.cn/music/photo_new/T001R150x150M000${mid}.jpg?max_age=2592000`
+	}
+}
--- vue-music/src/components/singer/singer.vue
+++ vue-music/src/components/singer/singer.vue
@@ -6,6 +6,10 @@
 
 <script>
 import {getSingerList} from 'api/singer.js'
+import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
+let HOT_NAME = '热门'
+let HOT_SINGER_LEN = 10
 export default {
   data(){
     return{
@@ -14,15 +18,63 @@ export default {
   },
   created(){
     this._getSingeList()
-    console.log(getSingerList);
   },
   methods:{
     _getSingeList(){
       getSingerList().then((res) => {
-        this.singers = res.data.list
-        console.log(this.singers);
-      })
+        if(res.code === ERR_OK){
+          this.singers = res.data.list
+          // console.log(this.singers);
 
+          console.log(this._normalizeSinger(this.singers));
+        }
+      })
+    },
+    _normalizeSinger(list){
+      let obj = {
+        hot:{
+          title:HOT_NAME,
+          items:[]
+        }
+      }
+
+      list.forEach((item, index) => {
+        if(index < HOT_SINGER_LEN){
+          obj.hot.items.push(new Singer({
+            id:item.Fsinger_id,
+            name:item.Fsinger_name,
+            mid:item.Fsinger_mid
+          }))
+        }
+        let key = item.Findex
+        if(!obj[key]){
+          obj[key] = {
+            title:key,
+            items:[]
+          }
+        }
+        obj[key].items.push(new Singer({
+          id:item.Fsinger_id,
+          name:item.Fsinger_name,
+          mid:item.Fsinger_mid
+        }))
+      })
+      // 为了得到有序列表，我们需要处理 obj
+      let hot = []
+      let ret = []
+      for(let key in obj){
+        let val = obj[key]
+        if(val.title.match(/[a-zA-Z]/)){
+          ret.push(val)
+        }else if(val.title === HOT_NAME){
+          hot.push(val)
+        }
+      }
+      ret.sort((a,b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      console.log(hot.concat(ret)[0].items);
+      return hot.concat(ret)
     }
   }
 }
